aec5c2e0a3c186c29edb110fd827f4a4d4ece250
ZachTheMighty <promoted66@gmail.com>
2026-03-26 11:25:23 +0000

    Added helper functions for the playRound function

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ function getComputerChoice() {
 function getHumanChoice() {
   return prompt("Rock Paper Scissors?");
 }
+
+function humanRock(computerChoice) {
+  switch (computerChoice) {
+    case "Rock":
+      return "Draw!";
+    case "Paper":
+      return "You lose! Paper beats Rock";
+    case "Scissors":
+      return "You win! Rock beats Scissors";
+  }
+}
+
+function humanPaper(computerChoice) {
+  switch (computerChoice) {
+    case "Rock":
+      return "You win! Paper beats Rock";
+    case "Paper":
+      return "Draw!";
+    case "Scissors":
+      return "You lose! Scissors beat Paper";
+  }
+}
+
+function humanScissors(computerChoice) {
+  switch (computerChoice) {
+    case "Rock":
+      return "You lose! Rock beats Scissors";
+    case "Paper":
+      return "You win! Scissors beat Paper";
+    case "Scissors":
+      return "Draw!";
+  }
+}
